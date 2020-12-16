5779cd39845fe4ded01fc83e42e34922e2fb51b2
futuran <s1711161@s.tsukuba.ac.jp>
2020-12-16 15:31:23 +0900

    multiencoder ver

--- onmt/models/model.py
+++ onmt/models/model.py
@@ -17,9 +17,9 @@ class NMTModel(nn.Module):
         super(NMTModel, self).__init__()
         self.encoder = encoder
         self.encoder2 = encoder2
-        self.sim_weight = torch.eye(512,requires_grad=True)
-        self.weight = torch.autograd.Variable(torch.Tensor([1]),requires_grad=True)
-        self.pooler = nn.AdaptiveAvgPool1d(10)
+        #self.sim_weight = torch.eye(512,requires_grad=True)
+        #self.weight = torch.autograd.Variable(torch.Tensor([1]),requires_grad=True)
+        #self.pooler = nn.AdaptiveAvgPool1d(10)
         self.decoder = decoder
 
         #print("model initialized!!")
@@ -49,10 +49,10 @@ class NMTModel(nn.Module):
         """
         dec_in = tgt[:-1]  # exclude last target from inputs
 
-        print("start batch")
-        print(src.size())
-        print(sim.size())
-        print(sim.dtype)
+        #print("start batch")
+        #print(src.size())
+        #print(sim.size())
+        #print(sim.dtype)
 
         src_enc_out, src_memory_bank, src_lens = self.encoder(src, src_lengths)
         sim_enc_out, sim_memory_bank, sim_lens = self.encoder2(sim, sim_lengths)
@@ -63,30 +63,30 @@ class NMTModel(nn.Module):
         sim_pooled_mb  = self.pooler(sim_memory_bank.transpose(2,0)).transpose(2,0)
         
 
-        print(self.sim_weight.device)
-        print(self.weight.device)
-        print(self.weight.grad)
-        sim_lineared_enc = torch.div(sim_pooled_enc, self.weight)
-        sim_lineared_mb  = torch.div(sim_pooled_mb,  self.weight)
+        #print(self.sim_weight.device)
+        #print(self.weight.device)
+        #print(self.weight.grad)
+        #sim_lineared_enc = torch.div(sim_pooled_enc, self.weight)
+        #sim_lineared_mb  = torch.div(sim_pooled_mb,  self.weight)
         #sim_lineared_enc = torch.bmm(sim_pooled_enc.transpose(0,1), self.sim_weight.expand(src.size()[1],512,512).to(src.device)).transpose(0,1)
         #sim_lineared_mb  = torch.bmm(sim_pooled_mb.transpose(0,1), self.sim_weight.expand(src.size()[1],512,512).to(src.device)).transpose(0,1)
 
-        src_out=torch.cat([torch.zeros(10,src.size()[1],src.size()[2],dtype=src.dtype, device=src.device),src])
+        #src_out=torch.cat([torch.zeros(10,src.size()[1],src.size()[2],dtype=src.dtype, device=src.device),src])
 
-        print(src_enc_out.size())
-        print(sim_lineared_enc.size())
-        print(torch.norm(self.weight))
+        #print(src_enc_out.size())
+        #print(sim_lineared_enc.size())
+        #print(torch.norm(self.weight))
 
-        enc_out = torch.cat([sim_lineared_enc, src_enc_out])
-        mb_out  = torch.cat([sim_lineared_mb,  src_memory_bank])
-
-
-        #src_out = torch.cat([sim,               src])
-        #enc_out = torch.cat([sim_enc_out,       src_enc_out])
-        #mb_out  = torch.cat([sim_memory_bank,   src_memory_bank])
+        #enc_out = torch.cat([sim_lineared_enc, src_enc_out])
+        #mb_out  = torch.cat([sim_lineared_mb,  src_memory_bank])
 
 
-        print(src_out.size())
+        src_out = torch.cat([sim,               src])
+        enc_out = torch.cat([sim_enc_out,       src_enc_out])
+        mb_out  = torch.cat([sim_memory_bank,   src_memory_bank])
+
+
+        #print(src_out.size())
 
         if bptt is False:
             self.decoder.init_state(src_out, mb_out, enc_out)
