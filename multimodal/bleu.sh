3b4207d04574b689cac119fc870be8b2c3f30af2
futuran <s1711161@s.tsukuba.ac.jp>
2020-12-18 02:18:13 +0900

    chnage conf

--- multimodal/bleu.sh
+++ multimodal/bleu.sh
@@ -1,3 +1,3 @@
 
 sed -r 's/(@@ )|(@@ ?$)//g' jaen2en/output.en > jaen2en/output.en.r
-perl /home/futuran/work/OpenNMT-py/tools/multi-bleu.perl ./data/NFR_FlickrCOCO_multi/merge.0.00/flickr_test.en1.tokenized.with_match.bpe.r < jaen2en/output.en.r > out.bleu
+perl /home/futuran/work/OpenNMT-py/tools/multi-bleu.perl ../../NFR_FlickrCOCO_multi/merge.0.00/flickr_test.en1.tokenized.with_match.bpe.r < jaen2en/output.en.r > out.bleu
